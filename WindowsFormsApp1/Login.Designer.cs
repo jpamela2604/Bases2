0f61102157a41ef6178cd27c16002482b8e80bad
Jenifer Pamela Palacios <jpamela.palacios@gmail.com>
2019-09-18 17:19:54 -0600

    agregue funcionalidad del login (verifica el usuario y password) si todo esta correcto devuelve el ROL de lo contrario pide credenciales d e nuevo

--- WindowsFormsApp1/Login.Designer.cs
+++ WindowsFormsApp1/Login.Designer.cs
@@ -43,6 +43,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(145, 22);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -62,6 +63,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Ingresar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
--- WindowsFormsApp1/Login.cs
+++ WindowsFormsApp1/Login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OracleClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace WindowsFormsApp1
 {
     public partial class Login : Form
     {
+        Conexion c = new Conexion();
         public Login()
         {
             InitializeComponent();
@@ -23,6 +25,37 @@ namespace WindowsFormsApp1
         }
 
         private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                OracleConnection ora = new OracleConnection("DATA SOURCE = orcl; PASSWORD=pampam; USER ID=SYSTEM;");
+
+                ora.Open();
+                OracleCommand comando = new OracleCommand("login", ora);
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                comando.Parameters.Add("usuario", OracleType.Number).Value = Convert.ToInt32(textBox1.Text);
+                comando.Parameters.Add("password", OracleType.VarChar).Value = textBox2.Text;
+                comando.Parameters.Add("resultado", OracleType.Number);
+                comando.Parameters["resultado"].Direction = ParameterDirection.ReturnValue;
+                comando.ExecuteNonQuery();
+                comando.Connection.Close();
+                /*ASIGNAR A VARIABLE DE CONFIGURACION*/
+                var codigoRol = Convert.ToString(comando.Parameters["resultado"].Value);                
+            }catch(Exception EX)
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                MessageBox.Show("usuario y/o contraseña invalidos");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
