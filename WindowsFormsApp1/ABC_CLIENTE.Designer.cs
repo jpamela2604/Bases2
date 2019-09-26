1ba4d4cf78fdd765c898877e267d25a58155c69f
AntonioAvalos <201114339@ingenieria.usac.edu.gt>
2019-09-26 17:36:54 -0600

    arreglar update picture CLIENTE

--- WindowsFormsApp1/ABC_CLIENTE.Designer.cs
+++ WindowsFormsApp1/ABC_CLIENTE.Designer.cs
@@ -523,6 +523,7 @@
             this.button4.TabIndex = 47;
             this.button4.Text = "Cambiar datos";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label14
             // 
@@ -559,6 +560,7 @@
             this.button5.TabIndex = 43;
             this.button5.Text = "Seleccionar";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -568,6 +570,7 @@
             this.button6.TabIndex = 42;
             this.button6.Text = "Seleccionar";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // label16
             // 
--- WindowsFormsApp1/ABC_CLIENTE.cs
+++ WindowsFormsApp1/ABC_CLIENTE.cs
@@ -29,9 +29,9 @@ namespace WindowsFormsApp1
         private void ABC_CLIENTE_Load(object sender, EventArgs e)
         {
             ora = new OracleConnection(
-                "DATA SOURCE = "+ Properties.Settings.Default.nombre_db + ";"+ 
-                "PASSWORD="+ Properties.Settings.Default.contrasenia_db + ";"+
-                "USER ID="+ Properties.Settings.Default.usuario_db + ";"
+                "DATA SOURCE = " + Properties.Settings.Default.nombre_db + ";" +
+                "PASSWORD=" + Properties.Settings.Default.contrasenia_db + ";" +
+                "USER ID=" + Properties.Settings.Default.usuario_db + ";"
                 );
 
             ArrayList t_cuenta = Get_Tipes_Client();
@@ -51,8 +51,9 @@ namespace WindowsFormsApp1
             combo_buscar.SelectedIndex = 1;
         }
 
-        private ArrayList Get_Tipes_Client() {
-            ArrayList tipos_usuario = new ArrayList();            
+        private ArrayList Get_Tipes_Client()
+        {
+            ArrayList tipos_usuario = new ArrayList();
             try
             {
                 ora.Open();
@@ -75,14 +76,16 @@ namespace WindowsFormsApp1
             {
                 System.Windows.Forms.MessageBox.Show("Error al buscar los tipos de cliente: " + e.Message);
             }
-            finally {
+            finally
+            {
                 ora.Close();
             }
-            
+
             return tipos_usuario;
         }
 
-        public byte[] convert_image_to_blob(String path) {
+        public byte[] convert_image_to_blob(String path)
+        {
             System.IO.FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
             BinaryReader br = new BinaryReader(fs);
             byte[] photo = br.ReadBytes((int)fs.Length);
@@ -91,15 +94,22 @@ namespace WindowsFormsApp1
             return photo;
         }
 
-        public String convert_blob_to_image(byte[] blob,String nombre) {
-            string path = Path.GetTempPath();
-            File.WriteAllBytes(@path +nombre+".jpg", blob);
-            return path+nombre+".jpg";
+        public String convert_blob_to_image(byte[] blob, String nombre)
+        {
+            String path = Path.GetTempPath();
+            String direccion = @path + nombre + ".jpg";
+            if (File.Exists(direccion))
+            {
+                File.Delete(direccion);
+            }            
+            File.WriteAllBytes(direccion, blob);
+            return direccion;
         }
 
-        public String[] Get_Client(int codigo_cliente ,int dpi) {
+        public String[] Get_Client(int codigo_cliente, int dpi)
+        {
             String[] s1 = new String[10];
-            DataTable tabla_tipos = new DataTable();
+            //DataTable tabla_tipos = new DataTable();
             try
             {
                 ora.Open();
@@ -108,7 +118,7 @@ namespace WindowsFormsApp1
                 comando.CommandType = CommandType.Text;
                 OracleParameter parametro1;
                 if (codigo_cliente != 0)
-                {              
+                {
                     comando.CommandText = "SELECT * from CLIENTE where codigo_cliente = :codigo_cliente";
                     parametro1 = new OracleParameter("codigo_cliente", OracleType.Int32);
                     parametro1.Value = codigo_cliente;
@@ -123,7 +133,7 @@ namespace WindowsFormsApp1
                 }
                 OracleDataReader lector = comando.ExecuteReader();
                 lector.Read();
-                
+
                 s1[0] = lector.GetInt32(0).ToString(); //codigo cliente
                 s1[1] = lector.GetString(1); //nombre
                 s1[2] = lector.GetString(2); //direccion
@@ -131,13 +141,14 @@ namespace WindowsFormsApp1
                 s1[4] = lector.GetInt32(4).ToString(); //telefono
                 s1[5] = lector.GetString(5); //correo
                 s1[6] = lector.GetInt32(6).ToString(); //tipo_cliente
-                byte[] foto = (byte [])(lector["foto"]); //foto
+                byte[] foto = (byte[])(lector["foto"]); //foto
                 s1[7] = convert_blob_to_image(foto, "foto"); //foto
-                byte[] firma = (byte [])(lector["firma"]); //firma
+                byte[] firma = (byte[])(lector["firma"]); //firma
                 s1[8] = convert_blob_to_image(firma, "firma"); //firma
                 s1[9] = lector.GetString(9); //dpi
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
                 System.Windows.Forms.MessageBox.Show("se produjo un error: " + e.Message);
             }
             finally
@@ -148,13 +159,14 @@ namespace WindowsFormsApp1
             return s1;
         }
 
-        private int Add_Client(String nombre, String direccion, int nit, int telefono, String correo, int tipo_cliente, byte[] foto, byte[] firma, String dpi) {
+        private int Add_Client(String nombre, String direccion, int nit, int telefono, String correo, int tipo_cliente, byte[] foto, byte[] firma, String dpi)
+        {
             int codigo_cliente = -1;
             try
             {
                 ora.Open();
                 comando = new OracleCommand("INSERT INTO CLIENTE (NOMBRE, DIRECCION, NIT, TELEFONO, CORREO, TIPO_CLIENTE,FOTO,FIRMA, DPI)" +
-                    "VALUES ('" + nombre + "','" + direccion + "','" + nit + "','" + telefono + "','" + correo + "','" + tipo_cliente + "',:iFOTO,:iFIRMA,'" + dpi + "')",ora);
+                    "VALUES ('" + nombre + "','" + direccion + "','" + nit + "','" + telefono + "','" + correo + "','" + tipo_cliente + "',:iFOTO,:iFIRMA,'" + dpi + "')", ora);
 
                 OracleParameter iFOTO = comando.Parameters.Add(":iFOTO", OracleType.Blob);
                 iFOTO.Value = foto;
@@ -163,8 +175,8 @@ namespace WindowsFormsApp1
                 iFIRMA.Value = firma;
 
                 comando.ExecuteNonQuery();
-                
-                comando = new OracleCommand("SELECT codigo_cliente from CLIENTE where dpi = "+dpi,ora);
+
+                comando = new OracleCommand("SELECT codigo_cliente from CLIENTE where dpi = " + dpi, ora);
                 adaptador = new OracleDataAdapter();
                 adaptador.SelectCommand = comando;
                 DataTable tabla_tipos = new DataTable();
@@ -176,7 +188,7 @@ namespace WindowsFormsApp1
                 }
                 System.Windows.Forms.MessageBox.Show("Operacion exitosa, su codigo de cliente: " + codigo_cliente);
             }
-            catch (Exception e )
+            catch (Exception e)
             {
                 System.Windows.Forms.MessageBox.Show("se produjo un error: " + e.Message);
             }
@@ -194,7 +206,7 @@ namespace WindowsFormsApp1
                 ora.Open();
                 comando = new OracleCommand();
                 comando.Connection = ora;
-                comando.CommandType = CommandType.Text;                
+                comando.CommandType = CommandType.Text;
                 comando.CommandText = "DELETE FROM CLIENTE where codigo_cliente = :codigo_cliente";
                 OracleParameter parametro1 = new OracleParameter("codigo_cliente", OracleType.Int32);
                 parametro1.Value = codigo;
@@ -212,11 +224,10 @@ namespace WindowsFormsApp1
                 box_bfoto.Text = "";
                 pic_bfoto.Image = null;
                 pic_bfirma.Image = null;
-
             }
-            catch(Exception e)
-            {            
-                System.Windows.Forms.MessageBox.Show("se produjo un error: "+e.Message);
+            catch (Exception e)
+            {
+                System.Windows.Forms.MessageBox.Show("se produjo un error: " + e.Message);
             }
             finally
             {
@@ -229,17 +240,28 @@ namespace WindowsFormsApp1
             try
             {
                 ora.Open();
-                comando = new OracleCommand("UPDATE CLIENTE " +
-                    "SET nombre = "+nombre+"," +
-                    "SET direccion = "+direccion+"," +
-                    "SET nit = "+nit+"," +
-                    "SET telefono = "+telefono+"," +
-                    "SET correo = "+correo+"," +
-                    "SET TIPO_CLIENTE = "+tipo_cliente+"," +
-                    "SET foto = "+foto+"," +
-                    "SET firma = "+firma+"," +
-                    "SET dpi = "+dpi
-                    +"WHERE codigo_cliente = "+codigo_cliente);
+                comando = new OracleCommand();
+                comando.Connection = ora;
+                comando.CommandType = CommandType.Text;
+                comando.CommandText =
+                "UPDATE CLIENTE SET" +
+                    " nombre = '" + nombre + "'," +
+                    " direccion = '" + direccion + "'," +
+                    " nit = '" + nit + "'," +
+                    " telefono = '" + telefono + "'," +
+                    " correo = '" + correo + "'," +
+                    " TIPO_CLIENTE = '" + tipo_cliente + "'," +
+                    " foto = :iFOTO," +
+                    " firma = :iFIRMA," +
+                    " dpi = '" + dpi + "'" +
+                    " WHERE codigo_cliente = " + codigo_cliente;
+
+                OracleParameter iFOTO = comando.Parameters.Add(":iFOTO", OracleType.Blob);
+                iFOTO.Value = foto;
+
+                OracleParameter iFIRMA = comando.Parameters.Add(":iFIRMA", OracleType.Blob);
+                iFIRMA.Value = firma;
+
                 comando.ExecuteNonQuery();
                 System.Windows.Forms.MessageBox.Show("Operacion exitosa, se han actualizado los datos del cliente : " + codigo_cliente);
             }
@@ -283,7 +305,6 @@ namespace WindowsFormsApp1
             }
         }
 
-
         private void textBox_KeyPress(object sender, KeyPressEventArgs e)
         {
             // Verify that the pressed key isn't CTRL or any non-numeric digit
@@ -291,7 +312,7 @@ namespace WindowsFormsApp1
             {
                 e.Handled = true;
             }
-            
+
         }
 
         private void but_enviar_Click(object sender, EventArgs e)
@@ -307,7 +328,7 @@ namespace WindowsFormsApp1
             if (combo_tipo.Text == "") { System.Windows.Forms.MessageBox.Show("debe seleccionar un tipo de cliente"); return; }
             if (box_foto.Text == "") { System.Windows.Forms.MessageBox.Show("debe agregar una foto"); return; }
             if (box_firma.Text == "") { System.Windows.Forms.MessageBox.Show("debe agregar una firma"); return; }
-            
+
             if (box_dpi.Text.Length != 13) { System.Windows.Forms.MessageBox.Show("la cantidad de digitos no corresponde a un dpi valido"); return; }
             if (box_telefono.Text.Length != 8) { System.Windows.Forms.MessageBox.Show("la cantidad de digitos no corresponde a un telefono valido"); return; }
 
@@ -326,7 +347,7 @@ namespace WindowsFormsApp1
             string tipo_cliente = ((KeyValuePair<string, string>)combo_tipo.SelectedItem).Key;
 
             int resultado = Add_Client(box_nombre.Text, box_direccion.Text, Int32.Parse(box_nit.Text), Int32.Parse(box_telefono.Text), box_correo.Text, Int32.Parse(tipo_cliente), foto, firma, box_dpi.Text);
-            if(resultado != -1)
+            if (resultado != -1)
             {
                 box_dpi.Text = "";
                 box_nombre.Text = "";
@@ -369,14 +390,26 @@ namespace WindowsFormsApp1
 
         private void btn_buscar_Click(object sender, EventArgs e)
         {
+            if(pic_bfoto.Image != null)
+            {
+                pic_bfoto.Image.Dispose();
+            }
+            if(pic_bfirma.Image != null)
+            {
+                pic_bfirma.Image.Dispose();
+            }            
+            
             if (box_cliente.Text == "") { System.Windows.Forms.MessageBox.Show("debe ingresar un codigo o dpi valido"); return; }
             if (combo_buscar.Text == "") { System.Windows.Forms.MessageBox.Show("debe seleccionar una opcion valida"); return; }
             int codigo = System.Convert.ToInt32(box_cliente.Text);
             String opcion = combo_buscar.Text;
             String[] resultado;
-            if (opcion == "CODIGO USUARIO") {
+            if (opcion == "CODIGO USUARIO")
+            {
                 resultado = Get_Client(codigo, 0);
-            } else {
+            }
+            else
+            {
                 resultado = Get_Client(0, codigo);
             }
             box_bcodigo.Text = resultado[0];//codigo de cliente
@@ -396,12 +429,58 @@ namespace WindowsFormsApp1
 
         private void button7_Click(object sender, EventArgs e)
         {
-            if (box_bcodigo.Text == "") {
+            if (box_bcodigo.Text == "")
+            {
                 System.Windows.Forms.MessageBox.Show("Debe selecionar un cliente para eliminarlo");
                 return;
             }
             Eliminar_Cliente(System.Convert.ToInt32(box_bcodigo.Text));
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int codigo_cliente = System.Convert.ToInt32(box_bcodigo.Text);
+            String nombre = box_bnombre.Text;
+            String direccion = box_bdireccion.Text;
+            int nit = System.Convert.ToInt32(box_bnit.Text);
+            int telefono = System.Convert.ToInt32(box_btelefono.Text);
+            String correo = box_bcorreo.Text;
+            byte[] foto = convert_image_to_blob(box_bfoto.Text);
+            byte[] firma = convert_image_to_blob(box_bfirma.Text);
+            int tipo_cliente = System.Convert.ToInt32(((KeyValuePair<string, string>)combo_tipo.SelectedItem).Key);
+            String dpi = box_bdpi.Text;
+            Edit_Client(codigo_cliente, nombre, direccion, nit, telefono, correo, tipo_cliente, foto, firma, dpi);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Subir Foto";
+            openFileDialog1.DefaultExt = "jpg";
+            //openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                box_bfoto.Text = openFileDialog1.FileName;
+                pic_bfoto.Image = Image.FromFile(box_bfoto.Text);
+                pic_bfoto.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Subir Firma";
+            openFileDialog1.DefaultExt = "jpg";
+            //openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                box_bfirma.Text = openFileDialog1.FileName;
+                pic_bfirma.Image = Image.FromFile(box_bfirma.Text);
+                pic_bfirma.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
     }
-    
+
 }
