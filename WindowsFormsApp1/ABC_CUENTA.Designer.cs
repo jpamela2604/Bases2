77722f27efdb4d401d8b2fa6782623b31c2f17f5
AntonioAvalos <201114339@ingenieria.usac.edu.gt>
2019-09-30 14:55:26 -0600

    crear cuenta

--- WindowsFormsApp1/ABC_CUENTA.Designer.cs
+++ WindowsFormsApp1/ABC_CUENTA.Designer.cs
@@ -31,16 +31,15 @@
             this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btn_crear = new System.Windows.Forms.Button();
+            this.box_saldo = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flow_cuentas = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -52,36 +51,80 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1074, 706);
+            this.tabControl1.Size = new System.Drawing.Size(1074, 597);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.button4);
-            this.tabPage1.Controls.Add(this.button3);
-            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.btn_crear);
+            this.tabPage1.Controls.Add(this.box_saldo);
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.button2);
-            this.tabPage1.Controls.Add(this.flowLayoutPanel1);
+            this.tabPage1.Controls.Add(this.flow_cuentas);
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1066, 677);
+            this.tabPage1.Size = new System.Drawing.Size(1066, 568);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "CREAR";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // btn_crear
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1066, 677);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "MODIFICAR";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.btn_crear.Location = new System.Drawing.Point(566, 456);
+            this.btn_crear.Name = "btn_crear";
+            this.btn_crear.Size = new System.Drawing.Size(106, 47);
+            this.btn_crear.TabIndex = 7;
+            this.btn_crear.Text = "Aceptar";
+            this.btn_crear.UseVisualStyleBackColor = true;
+            this.btn_crear.Click += new System.EventHandler(this.btn_crear_Click);
+            // 
+            // box_saldo
+            // 
+            this.box_saldo.Location = new System.Drawing.Point(215, 62);
+            this.box_saldo.Name = "box_saldo";
+            this.box_saldo.Size = new System.Drawing.Size(163, 22);
+            this.box_saldo.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(85, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // flow_cuentas
+            // 
+            this.flow_cuentas.AutoScroll = true;
+            this.flow_cuentas.Location = new System.Drawing.Point(202, 173);
+            this.flow_cuentas.Name = "flow_cuentas";
+            this.flow_cuentas.Size = new System.Drawing.Size(328, 184);
+            this.flow_cuentas.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero(s) de cuenta";
+            this.toolTip1.SetToolTip(this.label2, "Debe ingresar el numero de cliente al cual le pertenecerá la cuenta\r\n");
             // 
             // label1
             // 
@@ -94,81 +137,29 @@
             this.label1.Text = "Saldo inicial";
             this.toolTip1.SetToolTip(this.label1, "Debe ingresar un saldo para aperturar la cuenta");
             // 
-            // label2
+            // tabPage2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(60, 120);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(142, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Numero(s) de cuenta";
-            this.toolTip1.SetToolTip(this.label2, "Debe ingresar el numero de cliente al cual le pertenecerá la cuenta\r\n");
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1066, 677);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "MODIFICAR";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // toolTip1
             // 
             this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
             // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(202, 173);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(328, 184);
-            this.flowLayoutPanel1.TabIndex = 2;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(85, 225);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "-";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(85, 173);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "+";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(215, 62);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(163, 22);
-            this.textBox1.TabIndex = 6;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(566, 480);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Aceptar";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(60, 480);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "Limpiar";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
             // ABC_CUENTA
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1098, 730);
+            this.ClientSize = new System.Drawing.Size(1098, 619);
             this.Controls.Add(this.tabControl1);
             this.Name = "ABC_CUENTA";
             this.Text = "ABC_CUENTA";
+            this.Load += new System.EventHandler(this.ABC_CUENTA_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
@@ -186,9 +177,8 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel flow_cuentas;
+        private System.Windows.Forms.Button btn_crear;
+        private System.Windows.Forms.TextBox box_saldo;
     }
 }
--- WindowsFormsApp1/ABC_CUENTA.cs
+++ WindowsFormsApp1/ABC_CUENTA.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OracleClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace WindowsFormsApp1
 {
     public partial class ABC_CUENTA : Form
     {
+        OracleConnection ora;
+        OracleDataAdapter adaptador;
+        OracleCommand comando;
+
         public ABC_CUENTA()
         {
             InitializeComponent();
@@ -26,16 +31,81 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             TextBox txtRun = new TextBox();
-            txtRun.Name = "txtDynamic" + c++;
-            //txtRun.Location = new System.Drawing.Point(20, 18 + (20 * c));
+            txtRun.Name = "box_cuenta" + c++;
             txtRun.Size = new System.Drawing.Size(200, 25);
-            flowLayoutPanel1.Controls.Add(txtRun);
+            flow_cuentas.Controls.Add(txtRun);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             c--;
-            flowLayoutPanel1.Controls.RemoveByKey("txtDynamic" + c);
+            flow_cuentas.Controls.RemoveByKey("box_cuenta" + c);
+        }
+
+        private void btn_crear_Click(object sender, EventArgs e)
+        {
+            var saldo_disponible = box_saldo.Text;
+            var estado_cuenta = 1;
+            var tipo_cuenta = 1;
+            if (flow_cuentas.Controls.Count > 1) {
+                tipo_cuenta = 2;
+            }
+
+            ora.Open();
+            comando = new OracleCommand();
+            comando.Connection = ora;
+            OracleTransaction trans = ora.BeginTransaction();
+            comando.Transaction = trans;
+
+            try
+            {
+                //primero se crea la cuenta    
+                comando.CommandText = "INSERT INTO CUENTA (ESTADO, SALDO_DISPONIBLE, ESTADO_CUENTA, EN_RESERVA, TIPO_CUENTA) VALUES ('1', '"+saldo_disponible+"', '"+estado_cuenta+"', '0', '"+tipo_cuenta+"') RETURNING numero_cuenta into :variable2";
+                OracleParameter parametro1 = new OracleParameter("variable2", OracleType.Number);
+                parametro1.Direction = ParameterDirection.Output;
+                parametro1.Value = 0;
+                comando.Parameters.Add(parametro1);
+                comando.ExecuteNonQuery();
+                var variable2 = comando.Parameters["variable2"].Value.ToString();
+                
+                //por cada cliente se relaciona con la cuenta 
+                foreach (object obj_cuenta in flow_cuentas.Controls)
+                {
+                    TextBox box_cuenta = (TextBox)obj_cuenta;
+                    comando.Parameters.Clear();
+                    comando.CommandText = "INSERT INTO CLIENTE_CUENTA(CLIENTE, CUENTA) VALUES(:cliente,:cuenta)";
+                    OracleParameter cliente = new OracleParameter("cliente", OracleType.Number);
+                    cliente.Value = box_cuenta.Text;
+                    comando.Parameters.Add(cliente);
+                    OracleParameter cuenta = new OracleParameter("cuenta", OracleType.Number);
+                    cuenta.Value = variable2;
+                    comando.Parameters.Add(cuenta);
+                    comando.ExecuteNonQuery();
+                }
+                trans.Commit();
+                System.Windows.Forms.MessageBox.Show("Se ha creado la cuenta exitosamente no."+variable2);
+            }
+            catch (DataException ex) {
+                trans.Rollback();
+                System.Windows.Forms.MessageBox.Show("Ha ocurrido un error durante la transacción");
+            }
+            finally
+            {
+                ora.Close();
+            }
+
+
+            
+           
+        }
+
+        private void ABC_CUENTA_Load(object sender, EventArgs e)
+        {
+            ora = new OracleConnection(
+                "DATA SOURCE = " + Properties.Settings.Default.nombre_db + ";" +
+                "PASSWORD=" + Properties.Settings.Default.contrasenia_db + ";" +
+                "USER ID=" + Properties.Settings.Default.usuario_db + ";"
+                );
         }
     }
 }
