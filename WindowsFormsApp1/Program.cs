a142c941a34b3c148702c4ab5cd30388312fb0ce
AntonioAvalos <201114339@ingenieria.usac.edu.gt>
2019-11-20 12:46:40 -0600

    cambio a menu

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new NotaCredito());
+            Application.Run(new Login());
         }
     }
 }
